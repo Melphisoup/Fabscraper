a7f0d15c76cc08359b41c3514f31ac561d8e8142
Melphisoup <kmatt5445@gmail.com>
2024-10-10 08:26:09 -0400

    Broke after an update, big sad

--- scrapeyscrape.py
+++ scrapeyscrape.py
@@ -1,32 +1,33 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time 
-import random
 
 
 
 
-#Start the session
+
+service = Service()
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(service=service, options=options)
 
 URL = "https://fabrary.net/decks?tab=latest"
 driver.get(URL)
-#Wait for the front page to load
+
 time.sleep(10)
 page_source = driver.page_source
 
 
-#Find all the decks links
+
 desired = driver.find_elements(By.XPATH, '//a[@href]')
 des = driver.find_elements(By.CLASS_NAME, 'css-bveltq')
-#Move links into a list. 
+ 
 for d in des:
     deck_links = d.get_attribute('href')
     link_list = []
     link_list.append(deck_links)
-#Open file
+
 link_file = open("links.txt", 'a')
 
 with link_file as lf:
